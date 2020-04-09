69b543c398987afc299fab09d37e40186fb3ea1b
kaioGama <kaioeduardoescar@gmail.com>
2020-04-09 19:48:29 -0400

    [test]  add on heroku 2

--- src/components/category.jsx
+++ src/components/category.jsx
@@ -3,8 +3,7 @@ function Categorys(props) {
     return (
       <button
         key={aux.id}
-        className="flex flex-col mx-2 items-center justify-center"
-      >
+        className="flex flex-col mx-2 items-center justify-center">
         <div className="flex w-16 h-16 rounded-full  border-2 border-teal-400 bg-white">
           <img src={aux.src} alt={aux.name} />
         </div>
