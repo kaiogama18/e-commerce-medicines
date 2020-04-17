c8c2db5a2a76ebeffd24d4dd442d6e034a7e470d
kaioGama <kaioeduardoescar@gmail.com>
2020-04-17 14:35:21 -0400

    [teste] -- ok

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,4 @@
 import '../styles/styles.scss';
-import '../styles/mobile_styles.scss';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 
