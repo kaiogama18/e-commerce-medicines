602f1a220fd23f50fea9c8a65a2474599faca12b
KaioGama <kaioeduardoescar@gmail.com>
2020-07-01 19:33:36 -0400

    teste de navbar
    ajuste

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,56 +2,74 @@ import { Breakpoint } from 'react-socks';
 import Link from 'next/link';
 import { Search } from '..';
 import styles from "./Navbar.module.scss"
-import { Badge, Grid, Typography, Container, Divider, Button, ButtonGroup, AppBar, Toolbar, CardMedia } from '@material-ui/core';
+import { Badge, Grid, Typography, Container, Divider, Button, AppBar, Toolbar, CardMedia, Paper, useScrollTrigger, Slide } from '@material-ui/core';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import PropTypes from 'prop-types';
 
-class Navbar extends React.Component {
 
+function ElevationScroll(props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
 
-  render() {
-    const data = {
-      logo_src: '/logo-Wedo-mini.png',
-      logo_name: 'WEDO MARKET',
-      menu_hamburguer: 'menu',
-      shopping_cart: 'shopping_basket',
-      login_user: 'account_circle',
-    };
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
 
-    return (
-      <>
-        <div className={styles.navLogin}>
-          <Container >
-            <Grid container alignItems="center" spacing={3}>
-              <Grid item xs>
-                <Typography variant="overline"> BEM-VINDO, FAÇA SEU
+ElevationScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  window: PropTypes.func,
+};
+
+function Navbar(props) {
+
+  const data = {
+    logo_src: '/logo-Wedo-mini.png',
+    logo_name: 'WEDO MARKET',
+    menu_hamburguer: 'menu',
+    shopping_cart: 'shopping_basket',
+    login_user: 'account_circle',
+  };
+
+  return (
+    <>
+      <div className={styles.navLogin}>
+        <Container >
+          <Grid container alignItems="center" spacing={3}>
+            <Grid item xs>
+              <Typography variant="overline"> BEM-VINDO, FAÇA SEU
                   <Link href="#"> LOGIN </Link>
                   OU
                   <Link href="#"> CADASTRE-SE </Link>
-                </Typography>
-              </Grid>
-              <Grid container justify="center" item xs>
-                <Typography variant="overline"> Vendas sujeitas à análise e confirmação de dados </Typography>
-              </Grid>
-              <Grid container justify="flex-end" item xs>
-                <div className="flex">
-                  <Button variant="overline"> MEUS PEDIDOS </Button>
-                  <Divider orientation="vertical" flexItem className={styles.divider} />
-                  <Button variant="overline"> MINHA CONTA </Button>
-                </div>
-              </Grid>
+              </Typography>
             </Grid>
-          </Container>
-        </div>
-        <Divider />
-        <Breakpoint medium up>
+            <Grid container justify="center" item xs>
+              <Typography variant="overline"> Vendas sujeitas à análise e confirmação de dados </Typography>
+            </Grid>
+            <Grid container justify="flex-end" item xs>
+              <div className="flex">
+                <Button variant="overline"> MEUS PEDIDOS </Button>
+                <Divider orientation="vertical" flexItem className={styles.divider} />
+                <Button variant="overline"> MINHA CONTA </Button>
+              </div>
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
+      <Breakpoint medium up>
+        <ElevationScroll {...props}>
           <AppBar className={styles.navbar} variant="outlined" position="static">
             <Toolbar>
               <Container>
-                <Grid container alignItems="center" spacing={3}>
+                <Grid container justify="space-between" alignItems="center" spacing={3}>
                   <Grid item xs>
                     <Link href="/">
                       <CardMedia
-                      className={styles.imgLogo}
+                        className={styles.imgLogo}
                         component="img"
                         alt={data.logo_name}
                         image={data.logo_src}
@@ -62,58 +80,38 @@ class Navbar extends React.Component {
                   <Grid item xs={8}>
                     <Search />
                   </Grid>
-                  <Grid item container justify="flex-end" xs>
-                    <div className={styles.carItem}>
+                  <Grid item xs>
+                    <Paper variant="outlined" className={styles.carItem}>
                       <Badge className="mr-5" badgeContent={1} color="primary">
                         <ShoppingBasketIcon />
                       </Badge>
-                      <Typography variant="h6" style={{ fontWeight: 500 }}> R$ 0,00 </Typography>
-                    </div>
+                      <Typography variant="h6" style={{ fontWeight: 500 }}> R$ 1,00 </Typography>
+                    </Paper>
                   </Grid>
-
                 </Grid>
               </Container>
-
             </Toolbar>
-
           </AppBar>
+        </ElevationScroll>
+      </Breakpoint>
 
-          {/* <nav>
-            <div className="flex-center">
-              <Link href="/">
-                <img className={styles.imgLogo} src={data.logo_src} alt={data.logo_name} />
-              </Link>
-              <Search />
-            </div>
-            <div className="flex">
-              <div className={styles.carItem}>
-                <Badge className="mr-5" badgeContent={1} color="primary">
-                  <ShoppingBasketIcon />
-                </Badge>
-                <Typography variant="h6" style={{ fontWeight: 500 }}> R$ 0,00 </Typography>
-              </div>
-            </div>
-          </nav> */}
-
-        </Breakpoint>
-        <Breakpoint small down>
-          <nav>
-            <div className="flex-center">
-              <button className="material-icons">{data.menu_hamburguer}</button>
-            </div>
-            <div className="flex overflow-hidden items-center">
-              <Link href="/">
-                <img src={data.logo_src} alt={data.logo_name} />
-              </Link>
-            </div>
-            <div className="flex-center">
-              <button className="material-icons ">{data.shopping_cart}</button>
-            </div>
-          </nav>
-        </Breakpoint>
-      </>
-    );
-  }
+      <Breakpoint small down>
+        <nav>
+          <div className="flex-center">
+            <button className="material-icons">{data.menu_hamburguer}</button>
+          </div>
+          <div className="flex overflow-hidden items-center">
+            <Link href="/">
+              <img src={data.logo_src} alt={data.logo_name} />
+            </Link>
+          </div>
+          <div className="flex-center">
+            <button className="material-icons ">{data.shopping_cart}</button>
+          </div>
+        </nav>
+      </Breakpoint>
+    </>
+  );
 }
 
 export default Navbar;
