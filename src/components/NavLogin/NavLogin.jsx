6c680371e6dc456a73affb5b4fae873973638f55
KaioGama <kaioeduardoescar@gmail.com>
2020-07-29 19:40:24 -0400

    navLogins

--- src/components/NavLogin/NavLogin.jsx
+++ src/components/NavLogin/NavLogin.jsx
@@ -1,32 +1,30 @@
-import { Grid, Typography, Container, Divider, Button, Box, Dialog, Slide, DialogTitle, DialogContent, DialogContentText, TextField, InputAdornment, IconButton, FormControl, InputLabel, FilledInput, DialogActions } from '@material-ui/core';
+import { Grid, Typography, Container, Divider, Button, Box } from '@material-ui/core';
 import styles from "./NavLogin.module.scss"
 
 export default () => {
     return (
-        <>
-            <Box className={styles.navLogin}>
-                <Container >
-                    <Grid container alignItems="center" spacing={3}>
-                        <Grid item xs>
-                            <Typography variant="overline"> {data.welcome}
-                                <a> {data.login} </a> {"OU"}
-                                <a> {data.register} </a>
-                            </Typography>
-                        </Grid>
-                        <Grid container justify="center" item xs>
-                            <Typography variant="overline"> {data.warning} </Typography>
-                        </Grid>
-                        <Grid container justify="flex-end" item xs>
-                            <Box className="flex">
-                                <Button variant="overline"> {data.my_requests} </Button>
-                                <Divider orientation="vertical" flexItem className={styles.divider} />
-                                <Button variant="overline"> {data.my_account} </Button>
-                            </Box>
-                        </Grid>
+        <div className={styles.navLogins}>
+            <Container >
+                <Grid container alignItems="center" spacing={3}>
+                    <Grid item xs>
+                        <Typography variant="overline"> {data.welcome}
+                            <a> {data.login} </a> {"OU"}
+                            <a> {data.register} </a>
+                        </Typography>
                     </Grid>
-                </Container>
-            </Box>
-        </>
+                    <Grid container justify="center" item xs>
+                        <Typography variant="overline"> {data.warning} </Typography>
+                    </Grid>
+                    <Grid container justify="flex-end" item xs>
+                        <Box className="flex">
+                            <Button variant="overline"> {data.my_requests} </Button>
+                            <Divider orientation="vertical" flexItem className={styles.divider} />
+                            <Button variant="overline"> {data.my_account} </Button>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
     )
 }
 
