90b71137080f9e0a8732185c75cf94af59dcc5cd
KaioGama <kaioeduardoescar@gmail.com>
2020-08-04 01:51:51 -0400

    Ajuste
    para o campo de pesquisa

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,27 +3,33 @@ import styles from "./Search.module.scss"
 import { useEffect, useState, Fragment } from "react"
 import SearchIcon from '@material-ui/icons/Search';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import { TextField, CircularProgress } from "@material-ui/core";
+import { TextField, CircularProgress, InputAdornment } from "@material-ui/core";
 
 
 
 function Search() {
   const route = '/produto/consulta';
   const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
   const [options, setOptions] = useState([]);
+  const [inputValue, setInputValue] = useState('');
   const loading = open && options.length === 0;
 
   useEffect(() => {
     let active = true;
 
+    if (inputValue === '') {
+      setOptions(value ? [value] : []);
+      return undefined;
+    }
+
     if (!loading) {
-      console.log('[useEffect Search] ==> undefined!');
       return undefined;
     }
 
     (async () => {
-      const { data } = await Rota({ route, param: { "texto": "dorflex", "tipo": "des", "convenio": false } })
-      console.log('[Search] ==> ' + JSON.stringify(data, null, 2));
+      const { data } = await Rota({ route, param: { "texto": inputValue, "tipo": "des", "convenio": false } })
+      // console.log('[Search] ==> ' + JSON.stringify(data, null, 2));
       if (active) {
         setOptions(Object.keys(data).map((key) => data[key]));
       }
@@ -33,15 +39,13 @@ function Search() {
       active = false;
     };
 
-  }, [loading])
+  }, [value, inputValue, loading])
 
-  useEffect(() => {
-    if (!open) {
-      setOptions([]);
-    }
-  }, [open]);
-
-
+  // useEffect(() => {
+  //   if (!open) {
+  //     setOptions([]);
+  //   }
+  // }, [open]);
 
   return (
     <Autocomplete
@@ -53,33 +57,43 @@ function Search() {
       onClose={() => {
         setOpen(false);
       }}
+
       getOptionSelected={(option, value) => option.nome === value.nome}
       getOptionLabel={(option) => option.nome}
       options={options}
+      autoComplete
+      includeInputInList
+      filterSelectedOptions
+      value={value}
       loading={loading}
-      renderInput={(params) => (
-        <>
-          {/* <div className={styles.search}> */}
-          {/* <div className="mr-2">
-              <SearchIcon className={styles.searchIcon} />
-            </div> */}
+      onChange={(event, newValue) => {
+        setOptions(newValue ? [newValue, ...options] : options);
+        setValue(newValue);
+      }}
+      onInputChange={(event, newInputValue) => {
+        setInputValue(newInputValue);
+      }}
 
-          <TextField
-            {...params}
-            variant="outlined"
-            className={styles.search}
-            InputProps={{
-              ...params.InputProps,
-              endAdornment: (
-                <Fragment>
-                  {loading ? <CircularProgress color="inherit" size={20} /> : null}
-                  {params.InputProps.endAdornment}
-                </Fragment>
-              ),
-            }}
-          />
-          {/* </div> */}
-        </>
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          variant="outlined"
+          className={styles.search}
+          InputProps={{
+            ...params.InputProps,
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon className={styles.searchIcon} />
+              </InputAdornment>
+            ),
+            endAdornment: (
+              <Fragment>
+                {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                {params.InputProps.endAdornment}
+              </Fragment>
+            ),
+          }}
+        />
       )}
     />
   );
