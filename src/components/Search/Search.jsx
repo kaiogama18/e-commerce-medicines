70c89cdbb0f789c71501e7c7a9420d41695410a8
KaioGama <kaioeduardoescar@gmail.com>
2020-08-05 18:25:22 -0400

    Link Api
    já com os dados do produto pesquisado

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,8 +3,10 @@ import styles from "./Search.module.scss"
 import { useEffect, useState, Fragment } from "react"
 import SearchIcon from '@material-ui/icons/Search';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import { TextField, CircularProgress, InputAdornment } from "@material-ui/core";
-
+import { TextField, CircularProgress, InputAdornment, Grid, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+// import { parse } from "@babel/core";
+// import parse from 'autosuggest-highlight/parse';
 
 
 function Search() {
@@ -29,7 +31,6 @@ function Search() {
 
     (async () => {
       const { data } = await Rota({ route, param: { "texto": inputValue, "tipo": "des", "convenio": false } })
-      // console.log('[Search] ==> ' + JSON.stringify(data, null, 2));
       if (active) {
         setOptions(Object.keys(data).map((key) => data[key]));
       }
@@ -41,63 +42,128 @@ function Search() {
 
   }, [value, inputValue, loading])
 
-  // useEffect(() => {
-  //   if (!open) {
-  //     setOptions([]);
-  //   }
-  // }, [open]);
+
 
   return (
-    <Autocomplete
-      open={open}
-      onOpen={() => {
-        setOpen(true);
-      }}
-      onClose={() => {
-        setOpen(false);
-      }}
+    <>
+      {/* <div className="bg-gray-900 font-bold">{`inputValue: '${inputValue}'`}</div> */}
+      <Autocomplete
+        value={value}
+        open={open}
+        onOpen={() => {
+          setOpen(true);
+        }}
+        onClose={() => {
+          setOpen(false);
+        }}
+        getOptionLabel={((option) => (option.nome))}
+        options={options}
+        autoComplete
+        includeInputInList
+        filterSelectedOptions
+        loading={loading}
+        loadingText={"Carregando..."}
+        noOptionsText={"Produto não encontrado"}
+        onChange={(event, newValue) => {
+          setOptions(newValue ? [newValue, ...options] : options);
+          setValue(newValue);
+        }}
+        inputValue={inputValue}
+        onInputChange={(event, newInputValue) => {
+          setInputValue(newInputValue);
+        }}
 
-      getOptionSelected={(option, value) => option.nome === value.nome}
-      getOptionLabel={(option) => option.nome}
-      options={options}
-      autoComplete
-      includeInputInList
-      filterSelectedOptions
-      value={value}
-      loading={loading}
-      onChange={(event, newValue) => {
-        setOptions(newValue ? [newValue, ...options] : options);
-        setValue(newValue);
-      }}
-      onInputChange={(event, newInputValue) => {
-        setInputValue(newInputValue);
-      }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="outlined"
+            className={styles.search}
+            InputProps={{
+              ...params.InputProps,
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon className={styles.searchIcon} />
+                </InputAdornment>
+              ),
+              endAdornment: (
+                <Fragment>
+                  {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                  {params.InputProps.endAdornment}
+                </Fragment>
+              ),
+            }}
+          />
+        )}
+        renderOption={(option) => (
+          <Grid container alignItems="center">
+            <Grid item xs>
+              <Typography variant="body2" color="textSecondary">
+                {option.nome}
+              </Typography>
+            </Grid>
+          </Grid>
+        )
 
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          variant="outlined"
-          className={styles.search}
-          InputProps={{
-            ...params.InputProps,
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon className={styles.searchIcon} />
-              </InputAdornment>
-            ),
-            endAdornment: (
-              <Fragment>
-                {loading ? <CircularProgress color="inherit" size={20} /> : null}
-                {params.InputProps.endAdornment}
-              </Fragment>
-            ),
-          }}
-        />
-      )}
-    />
+        }
+      />
+      {/* <div className="bg-gray-900 font-bold">{`value: ${value !== null ? `'${JSON.stringify(value, null, 2)}'` : 'null'}`}</div> */}
+
+    </>
   );
 }
 
 export default Search;
 
 
+// return (
+//   <Autocomplete
+//     open={open}
+//     onOpen={() => {
+//       setOpen(true);
+//     }}
+//     onClose={() => {
+//       setOpen(false);
+//     }}
+
+//     getOptionSelected={(option, value) => option.nome === value.nome}
+//     getOptionLabel={((option) => (option.nome))}
+//     options={options}
+//     autoComplete
+//     includeInputInList
+//     filterSelectedOptions
+//     value={value}
+//     loading={loading}
+//     loadingText={"Carregando..."}
+//     noOptionsText={"Produto não encontrado"}
+//     onChange={(event, newValue) => {
+//       setOptions(newValue ? [newValue, ...options] : options);
+//       setValue(newValue);
+//     }}
+//     onInputChange={(event, newInputValue) => {
+//       setInputValue(newInputValue);
+//     }}
+
+//     renderInput={(params) => (
+//       <TextField
+//         {...params}
+//         variant="outlined"
+//         className={styles.search}
+//         onClick={() => console.log(`item clicked: ${"testst"}`)}
+//         InputProps={{
+//           ...params.InputProps,
+//           startAdornment: (  
+//             <InputAdornment position="start">
+//               <SearchIcon className={styles.searchIcon} />
+//             </InputAdornment>
+//           ),
+//           endAdornment: (
+//             <Fragment>
+//               {loading ? <CircularProgress color="inherit" size={20} /> : null}
+//               {params.InputProps.endAdornment}
+//             </Fragment>
+//           ),
+//         }}
+//       />
+//     )}
+//   />
+// );
